eb687597a354cdfbdf94e9af9dd3be5ef7dd3ec0
rafael.mafra <rafael.mafra@plus10.de>
2026-03-06 19:10:10 +0100

    feat: change sign button from "Assinar agora" to "Assinar"

--- apps/remix/app/components/general/envelope-editor/envelope-direct-sign-button.tsx
+++ apps/remix/app/components/general/envelope-editor/envelope-direct-sign-button.tsx
@@ -148,7 +148,7 @@ export const EnvelopeDirectSignButton = () => {
       disabled={!canDistribute}
     >
       <SendIcon className="mr-2 h-4 w-4" />
-      <Trans>Assinar Agora</Trans>
+      <Trans>Assinar</Trans>
     </Button>
   );
 };
